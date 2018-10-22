4f13e9293e48245c5ad8cf508270c53bbe2ba065
Peter Nagy <petnagy@gmail.com>
2018-10-22 16:57:38 +0200

    Add error dialogs

--- app/src/main/java/com/petnagy/superexchange/pages/fragments/currentrate/CurrentRateFragment.kt
+++ app/src/main/java/com/petnagy/superexchange/pages/fragments/currentrate/CurrentRateFragment.kt
@@ -3,9 +3,14 @@ package com.petnagy.superexchange.pages.fragments.currentrate
 import android.Manifest
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.net.Uri.fromParts
 import android.os.Bundle
+import android.provider.Settings
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,8 +21,11 @@ import com.petnagy.superexchange.pages.fragments.currentrate.viewmodel.CurrentRa
 import com.petnagy.superexchange.pages.fragments.currentrate.viewmodel.CurrentRateViewModelFactory
 import com.petnagy.superexchange.permission.PermissionManager
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.location_permission_need_dialog.view.*
 import timber.log.Timber
 import javax.inject.Inject
+import android.support.annotation.StringRes
+
 
 /***
  * Current Rate's Fragment.
@@ -78,21 +86,74 @@ class CurrentRateFragment : DaggerFragment() {
     }
 
     private fun showPermissionNeedDialog() {
+        activity?.let {
+            val builder = AlertDialog.Builder(it)
+            val view = layoutInflater.inflate(R.layout.location_permission_need_dialog, null)
+            builder.setView(view)
+                    .setNegativeButton(R.string.dialog_cancel_button) { dialog, _ ->
+                        dialog.cancel()
+                    }
+            val dialog = builder.show()
+            view.btn_settings_launch.setOnClickListener {
+                launchAskPermission()
+                dialog.dismiss()
+            }
+        }
+
+    }
+
+    private fun launchAskPermission() {
+        val intent = Intent()
+        intent.action = ACTION_APPLICATION_DETAILS_SETTINGS
+        intent.data = fromParts("package", activity?.packageName, null)
+        startActivity(intent)
     }
 
     private fun showPlayServiceErrorDialog() {
+        createSimpleAlertDialog(R.string.dialog_no_play_service_title, R.string.dialog_no_play_service_message)
     }
 
     private fun showSettingsErrorDialog() {
+        activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(R.string.dialog_setting_error_title)
+                    .setMessage(R.string.dialog_setting_error_message)
+                    .setNegativeButton(R.string.dialog_cancel_button) { dialog, _ ->
+                        dialog.cancel()
+                    }
+                    .setPositiveButton(R.string.dialog_ok_button) { dialog, _ ->
+                        launchLocationSettings()
+                        dialog.cancel()
+                    }
+        }
+    }
+
+    private fun launchLocationSettings() {
+        val viewIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivity(viewIntent)
     }
 
     private fun showLocationErrorDialog() {
+        createSimpleAlertDialog(R.string.dialog_location_error_title, R.string.dialog_location_error_message)
     }
 
     private fun showNetworkErrorDialog() {
+        createSimpleAlertDialog(R.string.dialog_network_error_title, R.string.dialog_network_error_message)
     }
 
     private fun showNotValidCountryCodeDialog() {
+        createSimpleAlertDialog(R.string.dialog_no_valid_country_code_title, R.string.dialog_no_valid_country_code_message)
+    }
+
+    private fun createSimpleAlertDialog(@StringRes titleResId: Int, @StringRes messageResId: Int) {
+        activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(titleResId)
+                    .setMessage(messageResId)
+                    .setNegativeButton(R.string.dialog_ok_button) { dialog, _ ->
+                        dialog.cancel()
+                    }
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
